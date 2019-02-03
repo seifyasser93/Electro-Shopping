f167780885101794931c8418fb54030f803600d1
Seif Yasser <sword.yasser@gmail.com>
2019-02-03 14:45:41 +0200

    my project setup is completed

---
+++ electroshoppingbackend/src/main/java/net/sy/electroshoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sy.electroshoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
